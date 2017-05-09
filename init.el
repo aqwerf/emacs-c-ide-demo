d6031e296619e9a17d0a97c7167a8023dde31ffb
Alan YS Lee <yslee@healcerion.com>
2017-05-09 21:00:07 +0900

    merge old settings

--- init.el
+++ init.el
@@ -23,8 +23,11 @@
 ;; (require 'setup-ggtags)
 (require 'setup-cedet)
 (require 'setup-editing)
+(require 'setup-rtags)
+;;(require 'setup-irony)
+(require 'setup-flycheck)
 
-
+(cmake-ide-setup)
 
 ;; function-args
 ;; (require 'function-args)
@@ -38,10 +41,62 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
+    (magit cmake-ide flycheck-irony flycheck-rtags flycheck company-irony company-irony-c-headers irony helm-rtags company-rtags rtags company-c-headers sr-speedbar function-args zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; set korean
+(set-language-environment "Korean")
+(prefer-coding-system 'utf-8)
+
+;; for copy & paste for MAC
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+(if (eq system-type 'darwin)
+    (setq interprogram-cut-function 'paste-to-osx))
+(if (eq system-type 'darwin)
+    (setq interprogram-paste-function 'copy-from-osx))
+
+;; swap 2 buffers
+(defun swap-buffers ()
+  "Put the buffer from the selected window in next window, and vice versa"
+  (interactive)
+  (let* ((this (selected-window))
+         (other (next-window))
+         (this-buf (window-buffer this))
+         (other-buf (window-buffer other)))
+    (when (not (equal this other))
+      (set-window-buffer other this-buf)
+      (set-window-buffer this other-buf)
+      (other-window 1)
+      )))
+
+(defun ask-compile ()
+  "Run compile in *compile* buffer"
+  (interactive)
+  (let ((buffer (get-buffer "*compilation*")))
+    (and buffer (switch-to-buffer-other-window buffer))
+    (compile (compilation-read-command compile-command))))
+
+;; key biding
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; magit
+(global-set-key (kbd "C-<tab>") 'helm-buffers-list)
+(global-set-key (kbd "C-x 9") 'swap-buffers)
+
+(global-set-key (kbd "C-c m") 'magit-status)
+
+(global-set-key (kbd "<f5>") 'recompile)
+(global-set-key (kbd "C-<f5>") 'ask-compile)
