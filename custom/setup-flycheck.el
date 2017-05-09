819deeb6bc1305819c53d2f681f839ffe6de0dde
Alan YS Lee <yslee@healcerion.com>
2017-05-09 20:53:35 +0900

    add auto complete mode (rtags, irony, and flycheck)

---
+++ custom/setup-flycheck.el
@@ -0,0 +1,16 @@
+(add-hook 'c++-mode-hook 'flycheck-mode)
+(add-hook 'c-mode-hook 'flycheck-mode)
+
+(require 'flycheck-rtags)
+
+(defun my-flycheck-rtags-setup ()
+  (flycheck-select-checker 'rtags)
+  (setq-local flycheck-highlighting-mode nil) ;; RTags creates more accurate overlays.
+  (setq-local flycheck-check-syntax-automatically nil))
+;; c-mode-common-hook is also called by c++-mode
+(add-hook 'c-mode-common-hook #'my-flycheck-rtags-setup)
+
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+
+(provide 'setup-flycheck)
---
+++ custom/setup-rtags.el
@@ -0,0 +1,15 @@
+(require 'rtags)
+(require 'company-rtags)
+
+(setq rtags-completions-enabled t)
+(eval-after-load 'company
+  '(add-to-list
+    'company-backends 'company-rtags))
+(setq rtags-autostart-diagnostics t)
+(rtags-enable-standard-keybindings)
+
+(require 'helm-rtags)
+(setq rtags-use-helm t)
+
+
+(provide 'setup-rtags)
